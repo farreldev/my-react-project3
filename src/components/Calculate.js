cca88e2858eac67f14184494c0e5f7d5748da9e9
office PC <teddywijaya93@gmail.com>
2023-01-11 17:34:10 +0700

    save reducer update

--- src/components/Calculate.js
+++ src/components/Calculate.js
@@ -3,21 +3,28 @@ import React, { useReducer } from 'react'
 function fnReducer(state, action) {
     switch (action.type) {
         case 'increment':
-            return state + 1;
+            return {count: state.count + action.payload};
         case 'decrement':
-            return state - 1;
+            return {count: state.count - action.payload};
         default:
             throw new Error('Unexpected Action!')
     }
 }
+
+const initState = {
+    count: 0,
+    theme: 'light'
+}
+
 export default function Calculate() {
-    const [count, setCount] = useReducer(fnReducer, 0);
+
+    const [state, dispatch] = useReducer(fnReducer, initState);
 
   return (
     <div className='flex items-center space-x-3 mb-3'>
-        <button onClick={() => setCount({type: 'decrement'})} className='border py-1 px-3'> - </button>
-        <span>{count}</span>
-        <button onClick={() => setCount({type: 'increment'})} className='border py-1 px-3'> + </button>
+        <button onClick={() => dispatch({type: 'decrement', payload: 2})} className='border py-1 px-3'> - </button>
+        <span>{state.count}</span>
+        <button onClick={() => dispatch({type: 'increment', payload: 2})} className='border py-1 px-3'> + </button>
     </div>
   )
 }
