de07da136050ed574bd8a5855f4fae49f0a87351
office PC <teddywijaya93@gmail.com>
2023-01-12 17:11:00 +0700

    save update semantic react code in page

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,13 @@ const About = () => {
             </div>
             <div className='flex flex-col items-start my-5'>
                 {/* <span className='capitalize block mb-2'>{context.theme}</span> */}
-                <button onClick={() => dispatch({type: 'toggletheme'})} className='border shadow-lg py-1 px-5'>Toggle to <span className='capitalize'>{state.theme === "light" ? "dark" : "light"}</span></button>
+                <button 
+                    onClick={() => dispatch({type: 'toggletheme'})} 
+                    className='border shadow-lg py-1 px-5'>Toggle to 
+                    <span 
+                        className='capitalize'>{state.theme === "light" ? "dark" : "light"}
+                    </span>
+                </button>
             </div>
             <h1>Ini adalah halaman About, klik <Link className='text-blue-500 font-medium underline' to="/about/team">disini</Link> untuk mengunjungi halaman team</h1>
         </div>
--- src/components/Settings.js
+++ src/components/Settings.js
@@ -3,6 +3,11 @@ export default function Settings() {
   const [state, dispatch] = useAppContext();
   console.log('user data: render');
   return (
-    <input onChange={(e) => dispatch({type: 'updateUser', payload:{...state.user, name: e.target.value}})} type="text" className='border p-2 w-full bg-gray-700 text-white dark:bg-white dark:text-gray-700 rounded shadow-lg' placeholder='Change Name' value={state.user.name ?? ''} />
+    <input 
+      onChange={(e) => dispatch({type: 'updateUser', payload:{...state.user, name: e.target.value}})} 
+      type="text" 
+      className='border p-2 w-full bg-gray-700 text-white dark:bg-white dark:text-gray-700 rounded shadow-lg' 
+      placeholder='Change Name' 
+      value={state.user.name ?? ''} />
   )
 }
